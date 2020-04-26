e46824e588e3526f92b21fd954501ab27836d04b
kirans4976 <kirans4976@gmail.com>
2020-04-26 22:51:53 +0530

    Added AysncAwait_P1

---
+++ 2.AysncAwait/MainWindow.xaml.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFUserInterface
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /*async is useful in 
+         * when u have a,b,c,d tasks out of which b task is  taking a bit longer
+         then async not blocks the user interface
+         the 2nd benfit comes when task c,d not rely on task B we can execute then in async
+         */
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void executeSync_Click(object sender, RoutedEventArgs e)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            RunDownloadSync();
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            resultsWindow.Text += $"Total execution time: { elapsedMs }";
+        }
+        /* in events u cnt use task that is exception
+         * u can use async void and awiat
+         */
+        private async void executeAsync_Click(object sender, RoutedEventArgs e)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            await RunDownloadParallelAsync();
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            resultsWindow.Text += $"Total execution time: { elapsedMs }";
+        }
+
+        private List<string> PrepData()
+        {
+            List<string> output = new List<string>();
+
+            resultsWindow.Text = "";
+
+            output.Add("https://www.yahoo.com");
+            output.Add("https://www.google.com");
+            output.Add("https://www.microsoft.com");
+            output.Add("https://www.cnn.com");
+            output.Add("https://www.codeproject.com");
+            output.Add("https://www.stackoverflow.com");
+
+            return output;
+        }
+
+        private async Task RunDownloadAsync()
+        {
+            List<string> websites = PrepData();
+
+            foreach (string site in websites)
+            {
+                // it will block next website until the 1st website done
+                // obs:optimize: never keep the await in foreach
+                //u can make entire method async thats great else ts.run(method()) both same st is preferrable
+                WebsiteDataModel results = await Task.Run(() => DownloadWebsite(site));
+                ReportWebsiteInfo(results);
+            }
+        }
+
+        /*
+         * if u dont return anything return type must be Task
+         if u return anything like string return type must be task<string>
+        * The method name must be Inculuded Async at the last & let evetyone know it is async
+        * Advanced is cancelling a mid task and getting some info back in the task run 
+        */
+        private async Task RunDownloadParallelAsync()
+        {
+
+            /*before RunDownloadAsync()
+             */
+            List<string> websites = PrepData();
+            List<Task<WebsiteDataModel>> tasks = new List<Task<WebsiteDataModel>>();
+
+            foreach (string site in websites)
+            {
+                // before task.run(()=>DownlloadWebsite())
+                tasks.Add(DownloadWebsiteAsync(site));
+            }
+
+            /* when all does to pass all the tasks wait until the
+             * all the tasks executes
+             * can be optimized but later which incluedes repoting back touser 
+             * interface which will be complicated
+             */
+            var results = await Task.WhenAll(tasks);
+
+            foreach (var item in results)
+            {
+                ReportWebsiteInfo(item);
+            }
+        }
+
+        private void RunDownloadSync()
+        {
+            List<string> websites = PrepData();
+
+            foreach (string site in websites)
+            {
+                WebsiteDataModel results = DownloadWebsite(site);
+                ReportWebsiteInfo(results);
+            }
+        }
+
+
+        //great simulator for sending request and getting data
+        private WebsiteDataModel DownloadWebsite(string websiteURL)
+        {
+            WebsiteDataModel output = new WebsiteDataModel();
+            WebClient client = new WebClient();
+
+            output.WebsiteUrl = websiteURL;
+            output.WebsiteData = client.DownloadString(websiteURL);
+
+            return output;
+        }
+
+
+        //Added new
+        private async Task<WebsiteDataModel> DownloadWebsiteAsync(string websiteURL)
+        {
+            WebsiteDataModel output = new WebsiteDataModel();
+            WebClient client = new WebClient();
+
+            output.WebsiteUrl = websiteURL;
+            output.WebsiteData = await client.DownloadStringTaskAsync(websiteURL);
+
+            return output;
+        }
+
+        private void ReportWebsiteInfo(WebsiteDataModel data)
+        {
+            resultsWindow.Text += $"{ data.WebsiteUrl } downloaded: { data.WebsiteData.Length } characters long.{ Environment.NewLine }";
+        }
+    }
+}
